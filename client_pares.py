bd5ec67eac307b3f0a225dcd389915f4adf0f366
Vitor Rodarte <vitorricoy@gmail.com>
2022-01-18 16:38:18 -0300

    Adicionado tratamento de erros no cliente

--- client_pares.py
+++ client_pares.py
@@ -1,5 +1,6 @@
 
 import grpc
+from grpc_status import rpc_status
 import sys
 from pares_pb2_grpc import ServerParesStub
 from pares_pb2 import RequisicaoInsercao, RequisicaoAtivacao, RequisicaoConsulta, RequisicaoTerminoPares
@@ -11,40 +12,45 @@ if __name__ == '__main__':
     with grpc.insecure_channel(enderecoServidor) as channel:  
         # Declara o Stub do servidor de pares usado pelo cliente
         stub = ServerParesStub(channel)
-        # Le linhas da entrada padrão até que um 'T' seja recebido
-        while True:
-            linha = input()
-            # Se a linha está vazia ele é ignorada
-            if len(linha) == 0:
-                continue
-            # Divide o comando e seus argumentos pelo separador
-            comandos = linha.split(',')
-            # Se o comando foi de inserção
-            if comandos[0] == 'I':
-                # Chama a função de inserir um novo par chave-valor no servidor de pares
-                resposta = stub.Insere(RequisicaoInsercao(chave=int(comandos[1]), valor=comandos[2]))
-                # Imprime o status da inserção
-                print(resposta.status)
-            # Se o comando foi de consulta
-            elif comandos[0] == 'C':
-                # Chama a função de consultar uma chave no servidor de pares
-                resposta = stub.Consulta(RequisicaoConsulta(chave=int(comandos[1])))
-                # Imprime o resultado da consulta
-                print(resposta.resultado)
-            # Se o comando foi de ativação
-            elif comandos[0] == 'A':
-                # Chama a função de ativação do servidor de pares
-                resposta = stub.Ativa(RequisicaoAtivacao(servico=comandos[1]))
-                # Imprime o número de chaves processadas pela ativação
-                print(resposta.chavesProcessadas)
-            # Se o comando foi de término
-            elif comandos[0] == 'T':
-                # Chama a função de terminar o servidor de pares
-                resposta = stub.Termina(RequisicaoTerminoPares())
-                # Imprime o status do comando de término
-                print(resposta.status)
-                # Encerra o cliente
-                exit(0)
-            else:
-                # Ignora mensagem invalida
-                continue
+        try:
+            # Le linhas da entrada padrão até que um 'T' seja recebido
+            while True:
+                linha = input()
+                # Se a linha está vazia ele é ignorada
+                if len(linha) == 0:
+                    continue
+                # Divide o comando e seus argumentos pelo separador
+                comandos = linha.split(',')
+                # Se o comando foi de inserção
+                if comandos[0] == 'I':
+                    # Chama a função de inserir um novo par chave-valor no servidor de pares
+                    resposta = stub.Insere(RequisicaoInsercao(chave=int(comandos[1]), valor=comandos[2]))
+                    # Imprime o status da inserção
+                    print(resposta.status)
+                # Se o comando foi de consulta
+                elif comandos[0] == 'C':
+                    # Chama a função de consultar uma chave no servidor de pares
+                    resposta = stub.Consulta(RequisicaoConsulta(chave=int(comandos[1])))
+                    # Imprime o resultado da consulta
+                    print(resposta.resultado)
+                # Se o comando foi de ativação
+                elif comandos[0] == 'A':
+                    # Chama a função de ativação do servidor de pares
+                    resposta = stub.Ativa(RequisicaoAtivacao(servico=comandos[1]))
+                    # Imprime o número de chaves processadas pela ativação
+                    print(resposta.chavesProcessadas)
+                # Se o comando foi de término
+                elif comandos[0] == 'T':
+                    # Chama a função de terminar o servidor de pares
+                    resposta = stub.Termina(RequisicaoTerminoPares())
+                    # Imprime o status do comando de término
+                    print(resposta.status)
+                    # Encerra o cliente
+                    exit(0)
+                else:
+                    # Ignora mensagem invalida
+                    continue
+        except grpc.RpcError as rpc_error:
+            status = rpc_status.from_call(rpc_error)
+            detalhes = status.details.join(', ')
+            raise RuntimeError('Cliente encerrado com o erro: %s' % detalhes)
